51c581c25214ce7509b8e61df85172729625b615
pollra <5346003-pollra32@users.noreply.gitlab.com>
2022-07-21 16:30:54 +0900

    update logout function

--- src/main/java/com/pollra/security/application/view/controller/LoginController.java
+++ src/main/java/com/pollra/security/application/view/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.pollra.security.application.view.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @since       2022.07.19
@@ -14,5 +20,16 @@ public class LoginController {
     @GetMapping("/login")
     public String login(){
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
     }
 }
